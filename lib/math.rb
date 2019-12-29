19bdbb4e4b03a359126ddff9384a9e529dd5e886
Erik Prenzlin <eprenzlin@gmail.com>
2019-12-29 15:08:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,21 @@ def addition(num1, num2)
   puts sum
 end
 
+addition(2,3)
+
 def subtraction(num1, num2)
   sub_numbers = num1 - num2
   puts sub_numbers
 end
 
+subtraction(2,3)
+
 def division(num1, num2)
     division_numb = num1 / num2 
     puts division_numb
 end
+
+division (10,2)
 
 def multiplication(num1, num2)
   multi_numb = num1 * num2
