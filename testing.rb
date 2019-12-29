e82571a2785e8fc7f1c3a7727e3b854978a291b8
Erik Prenzlin <eprenzlin@gmail.com>
2019-12-29 15:08:45 +0000

    Automatically backed up by Learn

--- testing.rb
+++ testing.rb
@@ -4,3 +4,30 @@ def addition(num1, num2)
 end
 
 addition(2,3)
+
+def subtraction(num1, num2)
+  sub_numbers = num1 - num2
+  puts sub_numbers
+end
+
+subtraction(2,3)
+
+def division(num1, num2)
+    division_numb = num1 / num2 
+    puts division_numb
+end
+
+division (10,2)
+
+def multiplication(num1, num2)
+  multi_numb = num1 * num2
+  puts multi_numb
+end
+
+def modulo(num1, num2)
+  
+end
+
+def square_root(num)
+
+end
