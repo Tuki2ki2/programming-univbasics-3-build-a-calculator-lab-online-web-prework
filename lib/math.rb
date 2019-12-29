a1579be4d3c1bb6c942f8f1baf8169e961d5ee0e
Erik Prenzlin <eprenzlin@gmail.com>
2019-12-29 14:51:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,14 +1,15 @@
 def addition(num1, num2)
-  
-
+  sum = num1 + num2 
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  sub_numbers = num1 - num2
+  puts sub_numbers
 end
 
 def division(num1, num2)
-
+    division_numb = 
 end
 
 def multiplication(num1, num2)
