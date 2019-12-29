89f66b58b6d518339dafc22f6b7af07782d4de74
Erik Prenzlin <eprenzlin@gmail.com>
2019-12-29 14:52:33 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,15 +9,17 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-    division_numb = 
+    division_numb = num1 / num2 
+    puts division_numb
 end
 
 def multiplication(num1, num2)
-
+  multi_numb = num1 * num2
+  puts multi_numb
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
